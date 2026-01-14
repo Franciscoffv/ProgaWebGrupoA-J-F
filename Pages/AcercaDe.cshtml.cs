774e48c0afcee61eab8c812c39f10ac099b18dfd
Jesh20 <qjeshua50952@ufide.ac.cr>
2026-01-13 20:01:46 -0600

    about

---
+++ Pages/AcercaDe.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace trabajoEnClase1.Pages
+{
+    public class AcercaDeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
